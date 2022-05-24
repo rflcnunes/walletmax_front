a29cfe2b8351888d9921edc34be86302229c6284
Rafael <rafaelcnunes@icloud.com>
2022-05-24 03:25:31 -0300

    Adjustment navbar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueTheMask from 'vue-the-mask'
+Vue.use(VueTheMask)
+
+import money from 'v-money'
+Vue.use(money, {precision: 4})
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
