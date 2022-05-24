6a99c878b41d149c7b6ed05c9c1edb39db67a80c
Rafael <rafaelcnunes@icloud.com>
2022-05-24 03:27:35 -0300

    Inserted navbar and adjustments

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="container">
+    <NavbarComponent></NavbarComponent>
     <div class="row align-items-center">
-      <b-form>
-        <b-form-input @submit="deposit"
-                      class="mb-3 col-sm"
-                      v-model="depositValue"
-                      placeholder="Enter debit value"></b-form-input>
+      <div>
+        <h5>Hello, {{ userName }}</h5>
+        <h5>Total balance: <money disabled class="inputBalance" v-model="totalBalance" v-bind="money"></money></h5>
+      </div>
+      <b-form class="w-50">
+        <b-input-group prepend="$" append=".00">
+          <b-form-input @submit="deposit"
+                        class="mb-3 col-sm"
+                        v-model="depositValue"
+                        placeholder="Enter debit value">
+          </b-form-input>
+        </b-input-group>
       </b-form>
-      <b-form>
-        <b-form-input @submit="withdraw"
-                      class="mb-3 col-sm"
-                      v-model="withdrawValue"
-                      placeholder="Enter credit value"></b-form-input>
+      <b-form class="w-50">
+        <b-input-group prepend="$" append=".00">
+          <b-form-input @submit="withdraw"
+                        class="mb-3 col-sm"
+                        v-model="withdrawValue"
+                        placeholder="Enter credit value">
+          </b-form-input>
+        </b-input-group>
       </b-form>
     </div>
-    <div>
+    <div class="row align-items-center mb-5">
       <b-button-group>
         <b-button type="submit" @click="deposit()">Debit</b-button>
         <b-button type="submit" @click="withdraw()">Credit</b-button>
@@ -24,35 +35,84 @@
              id="historicList"
              :current-page="currentPage"
              striped hover
-             :items="historic"></b-table>
+             :fields="fields"
+             :items="historic">
+      <template v-slot:cell(value)="data">
+<!--        v-bind:value="!!data.value.is_active"-->
+        <h6><money disabled class="inputBalance" v-model="data.value" v-bind="money"></money></h6>
+      </template>
+    </b-table>
     <b-pagination
         v-model="currentPage"
         :total-rows="rows"
         :per-page="perPage"
         aria-controls="historicList"
         align="center"
-    ></b-pagination>
+        class="paginate"
+    >
+      <template #first-text><span class="text-success">First</span></template>
+      <template #prev-text><span class="text-danger">Prev</span></template>
+      <template #next-text><span class="text-warning">Next</span></template>
+      <template #last-text><span class="text-info">Last</span></template>
+    </b-pagination>
   </div>
 </template>
 
 <script>
 import Cookie from "js-cookie";
+import moment from "moment";
+import { Money } from "v-money";
+import NavbarComponent from "@/components/NavbarComponent";
 
 const axios = require('axios').default;
 
 export default {
   name: "HomePageView",
+  components: {
+    Money,
+    NavbarComponent,
+  },
   data() {
     return {
       perPage: 10,
       currentPage: 1,
       historic: [],
+      totalBalance: '',
+      userName: '',
       depositValue: '',
       withdrawValue: '',
+      fields: [
+        {
+          key: 'id',
+          label: 'ID'
+        },
+        {
+          key: 'type',
+          label: 'TYPE'
+        },
+        {
+          key: 'value',
+          label: 'VALUE'
+        },
+        {
+          key: 'date',
+          label: 'DATE',
+          formatter: (value) => this.formatterDate(value),
+        },
+      ],
+      money: {
+        decimal: ',',
+        thousands: '.',
+        prefix: 'R$ ',
+        suffix: ' #',
+        precision: 2,
+        masked: false
+      }
     }
   },
   created() {
     this.getAll();
+    this.getBalance();
   },
   computed: {
     rows() {
@@ -60,6 +120,9 @@ export default {
     }
   },
   methods: {
+    formatterDate(value) {
+      return moment(value).format("DD/MM/YYYY");
+    },
     getAll() {
       const token = Cookie.get('_my_app_token');
       axios.get('http://localhost/api/balance', {
@@ -67,9 +130,26 @@ export default {
           'Authorization': "Bearer " + token,
         }
       }).then(response => {
-        console.log(response.data.data);
+        console.log(response.data);
 
         this.historic = response.data.data;
+
+      }).catch(function (error) {
+        console.log(error);
+      })
+    },
+
+    getBalance() {
+      const token = Cookie.get('_my_app_token');
+      axios.get('http://localhost/api/balance/json', {
+        headers: {
+          'Authorization': "Bearer " + token,
+        }
+      }).then(response => {
+        console.log(response.data.data.value);
+
+        this.userName = response.data.data.username;
+        this.totalBalance = response.data.data.value;
 
       }).catch(function (error) {
         console.log(error);
@@ -118,5 +198,9 @@ export default {
 </script>
 
 <style scoped>
-
+.inputBalance {
+  border: 0;
+  background: none;
+  color: black;
+}
 </style>
