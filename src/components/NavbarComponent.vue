8e69bd8898423b5935073e1938c714569d66a07f
Rafael <rafaelcnunes@icloud.com>
2022-05-24 03:27:05 -0300

    Created navbar

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-breadcrumb class="menu">
+    <b-breadcrumb-item>
+      <b-icon class="link-item-icon" icon="house-fill" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+      <router-link class="link-item" to="/homepage">Home</router-link>
+    </b-breadcrumb-item>
+    <b-breadcrumb-item>
+      <b-icon class="link-item-icon" icon="power" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+      <b-link class="link-item" @click.prevent="RemoveCookie">Exit</b-link>
+    </b-breadcrumb-item>
+  </b-breadcrumb>
+</template>
+
+<script>
+import Cookie from "js-cookie";
+
+export default {
+  name: "NavbarComponent",
+  methods: {
+    RemoveCookie() {
+      Cookie.remove('_my_app_token');
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu {
+  border-radius: 20px;
+  margin: 20px 0 20px 0;
+}
+
+.link-item-icon {
+  color: #2c3e50;
+}
+
+.link-item {
+  background: none;
+  color: black;
+  text-decoration: none;
+  margin-left: 10px;
+}
+
+</style>
