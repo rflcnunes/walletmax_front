c0649418570bb641b6f5deb6fa51d55d35bc3a2f
Rafael <rafaelcnunes@icloud.com>
2022-05-29 16:58:48 -0300

    Created LoginView with Atomic

---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="home">
+  <div class="container">
+    <o-login></o-login>
+  </div>
+</div>
+</template>
+
+<script>
+import OLogin from "@/components/organisms/OLogin";
+
+export default {
+  name: "LoginView",
+  components: {
+    OLogin
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
