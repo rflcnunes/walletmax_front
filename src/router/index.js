1dc022e82c0f4d2aa5e0dcc12b816facd5548773
Rafael <rafaelcnunes@icloud.com>
2022-05-29 17:17:58 -0300

    Adjustments in routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Guard from '../services/middleware';
-import HomeView from '../views/HomeView.vue'
 import HomePageView from "@/views/HomePageView";
+import LoginView from "@/views/LoginView";
+import ODebit from "@/components/organisms/ODebit";
 import PageNotFound from "@/views/PageNotFound";
 
 Vue.use(VueRouter)
@@ -10,8 +11,19 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/homepage',
     name: 'home',
-    component: HomeView
+    component: HomePageView,
+    beforeEnter: Guard.auth
+  },
+  {
+    path: '/debit',
+    name: 'debit',
+    component: ODebit
   },
   {
     path: '/about',
@@ -20,16 +32,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/homepage',
-    name: 'homepage',
-    component: HomePageView,
-    beforeEnter: Guard.auth,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "homepageview" */ '../views/HomePageView.vue')
   },
   {
     path: '/:catchAll(.*)*',
