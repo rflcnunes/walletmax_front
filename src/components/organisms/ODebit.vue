c694c23f12be72a385496f6016b5f25d548ffe6c
Rafael <rafaelcnunes@icloud.com>
2022-05-29 17:01:42 -0300

    Created debit organism modal

---
+++ src/components/organisms/ODebit.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-form class="w-50">
+      <b-input-group>
+        <b-form-input @submit="deposit"
+                      class="mb-3 col-sm"
+                      v-model="depositValue" v-money="money"
+                      placeholder="Enter debit value">
+        </b-form-input>
+      </b-input-group>
+    </b-form>
+    <b-button-group>
+      <b-button type="submit" @click="deposit()">Debit</b-button>
+    </b-button-group>
+  </div>
+</template>
+
+<script>
+import Cookie from "js-cookie";
+import { VMoney } from "v-money";
+import {default as axios} from "axios";
+
+export default {
+  name: "ODebit",
+  data() {
+    return {
+      depositValue: '',
+      money: {
+        decimal: ',',
+        thousands: '.',
+        prefix: 'R$ ',
+        suffix: ' #',
+        precision: 2,
+        masked: false
+      }
+    }
+  },
+  directives: {money: VMoney},
+  methods: {
+    deposit() {
+      const token = Cookie.get('_my_app_token');
+      console.log(token)
+      axios({
+        method: 'post',
+        url: 'http://localhost/api/debit',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer " + token,
+        },
+        data: {
+          value: this.depositValue.replace(/[^0-9,]*/g, '').replace(',', '.')
+        }
+      }).then(res => {
+        console.log(res);
+        window.location.reload()
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
