1c8eddf2578afa934d7ca2618bd4ea7211178ef8
Rafael <rafaelcnunes@icloud.com>
2022-05-29 17:16:31 -0300

    Adjustments in NavBarComponent

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -5,22 +5,67 @@
       <router-link class="link-item" to="/homepage">Home</router-link>
     </b-breadcrumb-item>
     <b-breadcrumb-item>
+      <b-icon class="link-item-icon" icon="arrow-down" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+      <b-link class="link-item" @click="$bvModal.show('modal-debit')">Debit</b-link>
+    </b-breadcrumb-item>
+    <b-breadcrumb-item>
+      <b-icon class="link-item-icon" icon="arrow-up" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+      <b-link class="link-item" @click="$bvModal.show('modal-credit')">Credit</b-link>
+    </b-breadcrumb-item>
+    <b-breadcrumb-item>
       <b-icon class="link-item-icon" icon="power" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
       <b-link class="link-item" @click.prevent="RemoveCookie">Exit</b-link>
     </b-breadcrumb-item>
+    <b-modal id="modal-debit">
+      <template #modal-header>
+        <h5>Make a debit</h5>
+      </template>
+
+      <template>
+        <o-debit></o-debit>
+      </template>
+
+      <template #modal-footer="{ close }">
+        <b-button size="sm" variant="danger" @click="close()">
+          Exit
+        </b-button>
+      </template>
+    </b-modal>
+    <b-modal id="modal-credit">
+      <template #modal-header>
+        <h5>Make a credit</h5>
+      </template>
+
+      <template>
+        <o-credit></o-credit>
+      </template>
+
+      <template #modal-footer="{ close }">
+        <b-button size="sm" variant="danger" @click="close()">
+          Exit
+        </b-button>
+      </template>
+    </b-modal>
+
   </b-breadcrumb>
 </template>
 
 <script>
 import Cookie from "js-cookie";
+import ODebit from "@/components/organisms/ODebit";
+import OCredit from "@/components/organisms/OCredit";
 
 export default {
   name: "NavbarComponent",
+  components: {
+    ODebit,
+    OCredit
+  },
   methods: {
     RemoveCookie() {
       Cookie.remove('_my_app_token');
       this.$router.push('/')
-    }
+    },
   }
 }
 </script>
@@ -28,7 +73,6 @@ export default {
 <style scoped>
 .menu {
   border-radius: 20px;
-  margin: 20px 0 20px 0;
 }
 
 .link-item-icon {
@@ -40,6 +84,10 @@ export default {
   color: black;
   text-decoration: none;
   margin-left: 10px;
+}
+
+.close {
+  display: none;
 }
 
 </style>
